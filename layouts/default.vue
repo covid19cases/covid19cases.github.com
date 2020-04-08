7f2b84721fae6ab0e3b6b2248fa3051ec5835401
Sean Chen <Sean.Chen@leocorn.com>
2020-04-08 13:22:28 -0400

    get stared the default layout

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+// Simple layout for Vuetify playground
+v-app
+  v-content
+    Nuxt/
+
+  v-footer(color="indigo" app padless)
+    v-layout(justify-center row wrap)
+      v-flex(py-3 text-center white--text xs12)
+        | &copy;{{new Date().getFullYear()}} - <strong><a href="https://leocorn.com">Leocornus LTD.</a></strong>
+</template>
+
+<script>
+export default {
+}
+</script>
