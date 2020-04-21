8a4d792e16319902a9f67e1808b0f6de4c4c5159
Sean Chen <Sean.Chen@leocorn.com>
2020-04-21 15:30:46 -0400

    play around the colors.

--- pages/index.vue
+++ pages/index.vue
@@ -65,10 +65,12 @@ v-container( grid-list-xs )
   // using toolbar for navigation
   v-toolbar( dense ).mb-2
     v-spacer
-    v-btn( icon to="/" nuxt )
+    v-btn( icon to="/" nuxt color="primary")
       v-icon mdi-table-large
-    v-btn( icon to="/lines" nuxt )
+    v-btn( icon to="/lines" nuxt color="primary")
       v-icon mdi-chart-line
+    v-btn( icon to="/mlines" nuxt color="primary")
+      v-icon mdi-chart-multiline
     v-spacer
 
   // data table card.
