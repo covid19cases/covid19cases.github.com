f9ba321316636a70c01ec8b824bfa624fac5d542
Sean Chen <Sean.Chen@leocorn.com>
2020-04-21 15:38:58 -0400

    introduce the nav bar component

---
+++ components/nav-bar.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+// using toolbar for navigation
+v-toolbar( dense ).mb-2
+  v-spacer
+  v-btn( icon to="/" nuxt color="primary")
+    v-icon mdi-table-large
+  v-btn( icon to="/lines" nuxt color="primary")
+    v-icon mdi-chart-line
+  v-btn( icon to="/mlines" nuxt color="primary")
+    v-icon mdi-chart-multiline
+  v-spacer
+</template>
+
+<script>
+export default {
+
+    name: "navbar",
+    props: []
+}
